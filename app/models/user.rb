c045479fea4d5f6796cb0a0e586563556eec2201
jciancio <jciancio7@gmail.com>
2015-10-17 19:02:33 -0400

    i never actually committed any of the changes, here they all are

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ActiveRecord::Base
   validates :name, :address, presence: :true
   validates :credit_card, numericality: { only_integer: true}, length: { minimum: 16, maximum: 16 }
-  validates :email, presence: :true, 
+  validates :email, uniqueness: true, presence: :true
 end
