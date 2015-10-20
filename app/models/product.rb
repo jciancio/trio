58760141866d5ce94477be2e4e8f95d4e1acb19f
Adrian Knapp <adrianknapp@Adrians-MacBook-Pro.local>
2015-10-20 11:06:10 -0400

    wired up view cart button from product page

--- app/models/product.rb
+++ app/models/product.rb
@@ -3,6 +3,7 @@ class Product < ActiveRecord::Base
 	validates :price_in_cents, numericality: { only_integer: true, greater_than: 0 }
 	has_many :reviews
   belongs_to :user
+  
 	
 	def formatted_price
 		price_in_dollars = price_in_cents.to_f / 100
