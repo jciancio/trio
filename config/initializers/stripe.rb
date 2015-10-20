02f4456f40f5eeb3b352a3800db2c1b1c8aa9678
jciancio <jciancio7@gmail.com>
2015-10-20 13:58:07 -0400

    got stripe working

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,6 +1,7 @@
+require 'stripe'
 Rails.configuration.stripe = {
-	:publishable_key => ENV['PUBLISHABLE_KEY'],
-	:secret_key => ENV['SECRET_KEY']
+	:publishable_key => 'pk_test_LTnMfF09mQqrXZIwJ4D19Qd7',
+	:secret_key => 'sk_test_PVuhWdovATDkYunzGcYxbyvl'
 }
 
 Stripe.api_key = Rails.configuration.stripe[:secret_key]
