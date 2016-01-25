d674b3a3a59ad4fd03c7f5cb616d4e32d815694c
Adrian Knapp <Aknapp1212@gmail.com>
2016-01-25 02:07:02 -0500

    this commit is an attempt to recover work that was not linked to GitHub in Error

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151016205431) do
+ActiveRecord::Schema.define(version: 20151017190633) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "pictures", force: :cascade do |t|
+    t.string   "title"
+    t.string   "url"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "products", force: :cascade do |t|
     t.string   "name"
